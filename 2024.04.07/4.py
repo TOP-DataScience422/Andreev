db75dfe009e121f2754404c9ebc3425927f673fc
Gennadiy <renardes@gmail.com>
2024-05-18 21:08:45 +0500

    2024.04.07: комментарии, правки, итоги

--- 2024.04.07/4.py
+++ 2024.04.07/4.py
@@ -1,10 +1,18 @@
 number = int(input("Введите трехзначное число: "))
+
+# ПЕРЕИМЕНОВАТЬ: цифра числа — digit
 first_term = number // 100
 second_term = number % 100 // 10
 third_term = number % 10
+
 terms_summary = first_term + second_term + third_term
 terms_multiplication = first_term * second_term * third_term
+
 print(f"Сумма цифр = {terms_summary}\nПроизведение цифр = {terms_multiplication}")
+
 # Введите трехзначное число: 333
 # Сумма цифр = 9
 # Произведение цифр = 27
+
+
+# ИТОГ: отлично 3/3
--- 2024.04.07/5.py
+++ 2024.04.07/5.py
@@ -1,9 +1,14 @@
 integer_part = input("Введите целую часть: ")
 fractional_part = input("Введите дробную часть: ")
-numberMiles = float(integer_part + '.' + fractional_part)
-numberKm = round(numberMiles * 1.61, 1)
-print(f"{numberMiles} миль = {numberKm} км")
+
+number_miles = float(integer_part + '.' + fractional_part)
+number_km = round(number_miles * 1.61, 1)
+
+print(f"{number_miles} миль = {number_km} км")
+
 # Введите целую часть: 15
 # Введите дробную часть: 7
 # 15.7 миль = 25.3 км
 
+
+# ИТОГ: отлично — 3/3
